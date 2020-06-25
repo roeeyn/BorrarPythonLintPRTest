1bc8dff7ff903650da4ddc405e39b13495457dc6
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-06-24 20:55:31 -0500

    Added new stuff

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-import datetime
 
 print("hola soy pedrito")
 for i in range(9):
-    cosa = 2
+    COSA = 2
     print(f"Hola soy pedirot { i }")
--- otra_app.py
+++ otra_app.py
@@ -3,4 +3,5 @@ import datetime
 print("hola soy pedrito")
 for i in range(9):
     cosa = 2
-    print(f"Hola soy modificado { i }")
+
+    print(f"Hola soy modificado { cosa + i }")
